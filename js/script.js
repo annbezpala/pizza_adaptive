d7de732b7edb57c75b1b7fb3d46cdf8070ee31f0
Ann <annagorova66@gmail.com>
2019-04-22 12:40:33 +0300

    add pizza

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$('#show').click(function(){
+    $('#show').css('display', 'none');
+    $('#hide').css('display', 'block');
+    $('.menu-list').css('display', 'block');
+});
+
+$('#hide').click(function(){
+    $('#hide').css('display', 'none');
+    $('#show').css('display', 'block');
+    $('.menu-list').css('display', 'none');
+});
+
+
